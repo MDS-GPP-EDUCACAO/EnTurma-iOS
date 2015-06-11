5aef713ff464075ce5c6212ffbb371a8ed2bda64
thiagotmb <tmb0710@gmail.com>
2015-06-10 21:10:09 -0300

    Adding Bridging Header for Objective C hybrid code

---
+++ EnTurma/EnTurma/EnTurma-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  
+//
+//  Created by Thiago-Bernardes on 6/10/15.
+//
+//
+
+#ifndef _BridgingHeader_h
+#define _BridgingHeader_h
+
+#import "AFNetworking.h"
+#import "GraphKit.h"
+
+#endif
