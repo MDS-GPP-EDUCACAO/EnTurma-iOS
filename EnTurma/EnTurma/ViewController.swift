be4a17eeaa3a1568d476b665e4ec8c4f95f54d35
Gabriel Araújo <gabrieldea.silva@gmail.com>
2015-06-16 08:04:15 -0300

    Removing sample request

--- EnTurma/EnTurma/ViewController.swift
+++ EnTurma/EnTurma/ViewController.swift
@@ -14,15 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        var params = ["year":"2008","state":"AC","grade":"1° ano","test_type":"Total","public_type":"Total","local":"Total"]
-        
-        var rest = RESTFullManager(params: params)
-        rest.requestReport({ (<#AFHTTPRequestOperation!#>, <#AnyObject!#>) -> Void in
-            <#code#>
-        }, failure: <#(AFHTTPRequestOperation!, NSError!) -> Void##(AFHTTPRequestOperation!, NSError!) -> Void#>)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
